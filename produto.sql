0bc971df792d53406f31f3679b9ce78348bccc50
mari <95420095+mwrina@users.noreply.github.com>
2024-05-19 14:07:23 -0400

    bd

--- produto.sql
+++ produto.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS `produto`
+DROP TABLE IF EXISTS `produto`;
 
-CREATE TABLE `produto` (
-    `cod_produto` INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE produto (
+    `cod_produto` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `nome_produto` VARCHAR(60),
     `preco_produto` FLOAT
-)
+);
 
 INSERT INTO `produto` (`nome_produto`, `preco_produto`) 
     VALUES ("exemplo01", 10.50), ("exemplo02", 15.25);
